ee686520fab8cc6852d00f2aefa82881da5ff7eb
mohamedlotfy <hammad.mohamed1893@gmail.com>
2024-11-24 18:38:16 +0100

    Release version 0.2.1

--- cloud_function_framework_tool/setup.py
+++ cloud_function_framework_tool/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloud-function-framework",
-    version="0.2.0",
+    version="0.2.1",
     author="Mohamed Hammad & Mohamed Abulazm",
     author_email="hammad.mohamed1893@gmail.com",
     description="A CLI tool to bootstrap Google Cloud Functions projects.",
